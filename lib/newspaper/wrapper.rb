be41bdbeecc04e03e242881168d51dea33064047
Andrew Kreps <andrew.kreps@gmail.com>
2015-05-04 00:59:14 -0700

    Well, that works.

--- lib/newspaper/wrapper.rb
+++ lib/newspaper/wrapper.rb
@@ -13,18 +13,30 @@ class String
     # rules: if there are 4 characters or less, move them to the next line.
     # Otherwise, hyphenate.
     return_str = []
+    push_forward = ''
+
     new_str.each_with_index do |elem, index|
+      unless push_forward.empty?
+        elem = push_forward + elem
+        push_forward = ''
+      end
+
       if index == new_str.length - 1  # Last element
         return_str.push elem
         break
       end
 
       if elem[-1] != ' ' and new_str[index+1][0] != ' '
-        if elem[-4..-1].include? ' '
-          elem[-4..-1].rindex ' '
+        if elem[-4..-1].include? ' '    # Check the last 4 characters for a space
+          spacey = (elem[-4..-1].rindex(' ')).to_i * -1      # If we found one, let's find it and split the word.
+          new_elem = elem[0..(spacey - 1)]
+          push_forward = elem[(spacey + 1)..-1]
+          elem = new_elem
+          puts "elem: #{elem} Push forward #{push_forward}"
+        else
+          elem += '-'
         end
 
-        elem += '-'
         # puts elem[-1] + "' '" + new_str[index+1][0]
 
       end
--- lib/newspaper/wrapper/version.rb
+++ lib/newspaper/wrapper/version.rb
@@ -1,5 +1,5 @@
 module Newspaper
   module Wrapper
-    VERSION = "0.0.0"
+    VERSION = '0.0.0'
   end
 end
